be3dec406aa34ee7f97b4798c12168f9e8e44270
luanatiuma <luanatiuma@gmail.com>
2023-06-01 11:21:25 -0300

    Criação da página para estudo de js

---
+++ javascript.js
@@ -0,0 +1,19 @@
+//**** Desafio Descodificadas ****
+let alerta = alert("Página carregada com sucesso!")
+
+let numero1 = 10
+let numero2 = 2
+
+let soma = parseInt(numero1) + parseInt(numero2)
+let divisao = parseInt(numero1) / parseInt(numero2)
+let multiplicacao = parseInt(numero1) * parseInt(numero2)
+
+console.log(soma, divisao, multiplicacao)
+
+if(numero1 == numero2) {
+    alert("Os dois números são iguais")
+}
+else {
+    alert("Os dois números são diferentes")
+}
+
---
+++ js.js
@@ -0,0 +1,22 @@
+console.log("Soma de dois números") // O comando console.log é como se fosse o "escreva" do portugol, porém só aparece a mensagem no console
+
+var primeiroNumero = prompt("Digite o primeiro número") //O comando prompt é como se fosse o "leia" do portugol
+var segundoNumero = prompt("Digite o segundo número") // O prompt aparece uma caixa para digitação na página
+
+var soma = parseInt(primeiroNumero) + parseInt(segundoNumero) // A função parseInt transforma a string em inteiro, já que tudo que é digitado no prompt vira string
+
+console.log("Soma: ", soma) //A concatenação pode ser feita tanto com "," quando com o "+"
+console.log("Tipo da variável: ", typeof soma)
+
+//Usar o let para declarar variável e não o var
+//Como o Js é uma linguagem dinamicamente tipada, não é necessário declarar o tipo de variável, diferentemente do portugol
+
+let contador = 0
+
+while(contador < 10) {
+    console.log(contador)
+    contador++
+    //é possível colocar o código console.log(contador++)
+    //se você quiser encrementar antes, ou seja, começar a contar do 1 e não do 0
+    //é só colocar console.log(++contador)
+}
